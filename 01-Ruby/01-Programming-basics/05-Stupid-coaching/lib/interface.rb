5b14cce69cf24aa3e7c6609ebff9abeb1690b08a
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-08 14:56:49 +0200

    1st try at stupid coaching

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,14 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+def user_interface
+  user_input = 0
+  until user_input == "I am going to work right now SIR !"
+    puts "What you do want to say to your coach ?"
+    user_input = gets.chomp
+    coach_answer(user_input)
+  end
+end
+
+user_interface
+
