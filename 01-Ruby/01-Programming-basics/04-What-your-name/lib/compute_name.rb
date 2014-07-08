e099b3934c9385e7ddb01bcdb4aeb43b2b8d9894
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-08 14:23:14 +0200

    1st try at the exercise

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
@@ -1,3 +1,5 @@
 def compute_name(first_name, middle_name, last_name)
   # TODO: return full name using string interpolation
+  f_name = "#{first_name} #{middle_name} #{last_name}"
+  return f_name
 end
--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,9 +1,9 @@
 require_relative "compute_name"
 
 def ask_and_get(param)
-  #TODO:
-  #- Ask the user to write a value for param
-  #- Get and return the string inputted by the user
+  puts "Give me your #{param}"
+  name = gets.chomp
+  return name
 end
 
 def name_from_terminal
@@ -19,5 +19,7 @@ full_name = name_from_terminal
 #TODO: use string concatenation to build a custom message embedding the computed full_name
 
 # custom_message =
+
+custom_message = "Hello" + " " + full_name + " " + "!"
 
 puts custom_message
