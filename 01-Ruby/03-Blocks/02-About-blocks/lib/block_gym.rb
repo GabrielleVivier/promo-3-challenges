359b5bd53cb8944c53b82b2328c106fb6f773e53
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-10 12:36:57 +0200

    Yeah

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,12 +1,24 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  content = yield()
+  if attr.nil?
+    "<#{tag_name}>" + content + "</#{tag_name}>"
+  else
+    "<#{tag_name} #{attr[0]}='#{attr[1]}'>" + content + "</#{tag_name}>"
+  end
 end
 
 def timer_for
   #TODO:  Return time taken to execute the given block
+  start_time = Time.now
+  yield
+  end_time = Time.now - start_time
 end
 
 def transform(element)
   #TODO:  Simply execute the given block on element
+  yield(element)
 end
+
+
