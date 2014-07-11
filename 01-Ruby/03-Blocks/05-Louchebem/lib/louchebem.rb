725444f2643dce6d1ef9a2c7c4e079539a68404d
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-11 10:49:03 +0200

    works with punctutaion

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,15 +1,10 @@
 # Encoding: utf-8
 
 #
-def louchebemize(sentence)
-  #TODO: implement your louchebem translator
-  sentence.split(" ").map(&:louchebemize_word).join(' ')
-end
 
 def louchebemize_word(word)
   suffixe = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
   vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"] # %w(a e i o u y A E I O U Y)
-  special_characters = [""]
   if word.length == 1
     word
   elsif vowels.include?(word[0])
@@ -20,4 +15,19 @@ def louchebemize_word(word)
   end
 end
 
-puts louchebemize_word("charlie")
+def louchebemize_word_and_punct(word)
+  if word.index(/[[:punct:]]/).nil?
+    louchebemize_word(word)
+  else
+    punct_index = word.index(/[[:punct:]]/)
+    louchebemize_word(word[0..(punct_index-1)]) + word[punct_index..(word.size-1)]
+  end
+end
+
+def louchebemize(sentence)
+  #TODO: implement your louchebem translator
+  translated_words = sentence.split(" ").map do |word|
+    louchebemize_word_and_punct(word)
+  end
+  translated_words.join(" ")
+end
