1327a220c27d0b33c0c23ded79e32bcea0299b7e
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-13 20:17:33 +0200

    I think it works using regular expressions

--- 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
@@ -1,9 +1,14 @@
 def palindrome?(a_string)
   #TODO: check if a_string is a palindrome
+  array = a_string.downcase.split(//)
+  array = array.select { |x| /[[:alpha:]]/.match(x) }
+  array == array.reverse
 end
 
-  
+
 # Testing palindrome? function
 puts "#{palindrome?("A man, a plan, a canal -- Panama")}" #=> true
 puts "#{palindrome?("Madam, I'm Adam!")}" # => true
 puts "#{palindrome?("Abracadabra")}" # => false
+puts "#{palindrome?("Esope reste ici et se repose")}"
+puts "#{palindrome?("La mariee ira mal")}"
