dd9204c16e37aad6e10a77266a050717874746b4
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-14 19:21:34 +0200

    adding downcase

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,11 +1,11 @@
 def group_anagrams(words)
   #TODO: group anagrams
-  hash = words.group_by { |word| word.split(//).sort }
+  hash = words.group_by { |word| word.downcase.split(//).sort }
   hash.values
 end
 
 # input:
-p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
+p group_anagrams( ['Cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
 # =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
 
