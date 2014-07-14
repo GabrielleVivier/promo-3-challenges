1143f62dab123696506e5be0fc58f570d440642c
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-14 15:56:12 +0200

    Easy one

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,25 @@
 def beersong(nb_at_start)
   #TODO: sing the song
+  for i in (0..nb_at_start-1)
+    if nb_at_start-i > 1
+      puts "#{nb_at_start-i} bottles of beer on the wall, #{nb_at_start-i} bottles of beer!" + "\n"
+      puts "Take one down, pass it around, #{nb_at_start-i-1} bottles of beer on the wall!"
+    else
+      puts "1 bottle of beer on the wall, 1 bottle of beer!" + "\n"
+      puts "Take one down, pass it around, no more bottles of beer on the wall!"
+    end
+  end
 end
+
+beersong(5)
+
+#> 5 bottles of beer on the wall, 5 bottles of beer!
+#> Take one down, pass it around, 4 bottles of beer on the wall!
+#> 4 bottles of beer on the wall, 4 bottles of beer!
+#> Take one down, pass it around, 3 bottles of beer on the wall!
+#> 3 bottles of beer on the wall, 3 bottles of beer!
+#> Take one down, pass it around, 2 bottles of beer on the wall!
+#> 2 bottles of beer on the wall, 2 bottles of beer!
+#> Take one down, pass it around, 1 bottle of beer on the wall!
+#> 1 bottle of beer on the wall, 1 bottle of beer!
+#> Take one down, pass it around, no more bottles of beer on the wall!
