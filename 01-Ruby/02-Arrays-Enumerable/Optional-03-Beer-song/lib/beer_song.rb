0b0d0fc4be148e7ae55a152bd2e40a4a83f9b6fa
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-14 17:35:13 +0200

    added the argument stuff

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,5 +1,10 @@
-def beersong(nb_at_start)
+
   #TODO: sing the song
+ARGV[0].to_i
+nb_at_start = ARGV[0].to_i
+if ARGV.empty?
+  puts "No argument"
+else
   for i in (0..nb_at_start-1)
     if nb_at_start-i > 1
       puts "#{nb_at_start-i} bottles of beer on the wall, #{nb_at_start-i} bottles of beer!" + "\n"
@@ -11,7 +16,8 @@ def beersong(nb_at_start)
   end
 end
 
-beersong(5)
+
+
 
 #> 5 bottles of beer on the wall, 5 bottles of beer!
 #> Take one down, pass it around, 4 bottles of beer on the wall!
