8e26a23be27a5c621d2c34fefb891d3b8b3368c5
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-14 17:23:37 +0200

    3 errors - but it works

--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
@@ -3,13 +3,32 @@
 
 def add_to_cart(cart, product)
   # TODO: you get a cart and a product. Add the product to the cart!
+  cart << product
 end
 
 def cart_to_s(cart)
   # TODO: you get a cart, return a coma-separated String list of the contained products
+  string = ""
+  cart.each_with_index do |product, index|
+    if index == cart.count - 1
+      string += "#{product}"
+    else
+      string += "#{product},"
+    end
+  end
+  return string
 end
 
 def cart_total_price(cart, store_items)
   # TODO: you get a cart and the store items with their prices. Return the total
   # price of the shopping cart
+  total_price = 0
+  cart.each do |product|
+    if price_of_product(product).nil?
+      total_price += 0
+    else
+      total_price += price_of_product(product)
+    end
+  end
+  return total_price
 end
--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
@@ -3,33 +3,37 @@
 
 
 def store_items
-  # Here's what you should have in store:
-  #   yogurts : 2€
-  #   meat : 7€
-  #   vegetables: 5€
-  #   potatoes : 2€
-  #   rice : 1€
-
-  # TODO: find the correct data structure to hold the shopping store items
-  # instead of using nil below
-  store = nil
-
+  store = {
+    yogurts: 2,
+    meat: 7,
+    vegetables: 5,
+    potatoes: 2,
+    rice: 1
+  }
   return store
 end
 
 def is_product_in_store?(product)
-  # TODO: Check whether product is present in the store items
+  store_product = store_items
+  store_product.has_key?(product.to_sym.downcase)
 end
 
 def price_of_product(product)
-  # TODO: Return the price of the given product, if it's in store. nil otherwise
+  price = 0
+  if is_product_in_store?(product)
+    store_product = store_items
+    price += store_product[product.to_sym]
+  else
+    price = nil
+  end
+  return price
 end
 
 def store_items_to_s
-  # TODO: Return a String with all store items in the following form:
-  # - Meat: 7€
-  # - Vegetables: 5€
-  # ...
-  #
-  # TIP: add line breaks in your String with "\n"
+  string = ""
+  store_product = store_items
+  store_product.each do |key, value|
+    string += "- #{key}: #{value}€ \n"
+  end
+  puts string
 end
