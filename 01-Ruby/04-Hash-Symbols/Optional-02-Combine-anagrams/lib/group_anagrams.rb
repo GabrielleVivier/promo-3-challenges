960bad474a6761f7490516650ddd0a48080d93b0
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-14 19:10:00 +0200

    discovered group_by

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,8 +1,12 @@
 def group_anagrams(words)
   #TODO: group anagrams
+  hash = words.group_by { |word| word.split(//).sort }
+  hash.values
 end
 
 # input:
 p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
 # =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
+
+
