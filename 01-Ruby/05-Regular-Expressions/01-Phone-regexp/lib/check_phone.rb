92bde4df3362f70fcc487ed6ea7e3e327b2eb4fc
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-15 19:26:44 +0200

    tried differents ways

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,9 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  phone_number.gsub(/\-|\s/, '') =~ /^(\+336|06)\d{8}$/ ? true : false
 end
+
+
+#OR
+#phone_number.gsub(/\-|\s/, '') =~ /^(\+336|06)[1-9]\d{7}$/ ? true : false
+#phone_number =~ /^(\+33|0)\D?6(\D?\d){8}$/ ? true : false
