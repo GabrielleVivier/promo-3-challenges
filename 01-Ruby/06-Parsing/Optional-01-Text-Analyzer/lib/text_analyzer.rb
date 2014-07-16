17297e2a4a90063b10667ef0ddccd3267d597ba4
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-16 18:53:16 +0200

    just started

--- 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -1,3 +1,15 @@
-def analyze(filename)
+
+
+def analyze(file_name)
   #TODO: should analyze the text in the given file
+  file_path = File.dirname(__FILE__) + "/#{file_name}"
+  line_count = 0
+  file_string = ""
+  File.open(file_path, "r").each_line do |line|
+    line_count += 1
+    file_name + line.split(//).to_s
+  end
+  print file_name
 end
+
+analyze("text-to-analyse.txt")
