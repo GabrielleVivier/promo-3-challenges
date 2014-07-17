7c6489502321d667b3d96dc37cf75e03860ac12c
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-17 09:31:21 +0200

    just started with line_count - no check done yet

--- 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -1,15 +1,13 @@
 
 
-def analyze(file_name)
+def counting_lines(file_name)
   #TODO: should analyze the text in the given file
   file_path = File.dirname(__FILE__) + "/#{file_name}"
   line_count = 0
-  file_string = ""
   File.open(file_path, "r").each_line do |line|
     line_count += 1
-    file_name + line.split(//).to_s
   end
-  print file_name
+  print line_count
 end
 
 analyze("text-to-analyse.txt")
