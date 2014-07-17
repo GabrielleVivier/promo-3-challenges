3c6d7b335d45c1faae81a92d4b91b739c0fb3f24
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-17 11:35:26 +0200

    used inject for the sum

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,30 @@
 class Restaurant
-  
+
   #TODO add relevant accessors if necessary
-  
+
+  attr_reader :average_rating, :city
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating = 0
+    @ratings = []
   end
-  
+
   #TODO: implement #filter_by_city and #rate methods
-  
+
+  def self.filter_by_city(restaurants, city)
+    restaurants_list = []
+    restaurants.each do |restaurant|
+    restaurants_list << restaurant if restaurant.city == city
+    end
+    restaurants_list
+  end
+
+  def rate(rating)
+    @ratings << rating
+    @average_rating = (@ratings.inject(:+)) / (@ratings.size)
+  end
+
 end
