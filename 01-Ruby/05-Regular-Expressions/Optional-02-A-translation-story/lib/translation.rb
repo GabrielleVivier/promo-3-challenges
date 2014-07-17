4bf540a96b1275f1cef8cbff96264b708dfd398c
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-17 18:04:45 +0200

    refactored with gab

--- 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
@@ -1,30 +1,43 @@
-STRINGS = {
- home: {
-   intro: {
-     en: 'Welcome on Le Wagon',
-     fr: 'Bienvenue sur Le Wagon'
-   },
-   content: {
-     explanation: {
-       en: 'This is an interesting exercise',
-       fr: 'C\'est un exercice interessant',
-       de: 'Es ist eine interesante Ãbung'
-     },
-     goodbye: {
-       en: 'Goodbye',
-       fr: 'Au revoir',
-       es: 'Adios'
-     }
-   }
- }
-}
+require 'awesome_print'
 
-def getTranslation(a_string, a_language)
-  # your code goes here
+ STRINGS = {
+    home: {
+      intro: {
+        en: 'Welcome on Le Wagon',
+        fr: 'Bienvenue sur Le Wagon'
+      },
+      content: {
+        explanation: {
+        en: 'This is an interesting exercise',
+        fr: 'C\'est un exercice interessant',
+        de: 'Es ist eine interesante Ãbung'
+        },
+      goodbye: {
+        en: 'Goodbye',
+        fr: 'Au revoir',
+        es: 'Adios'
+        }
+      }
+    }
+  }
+
+def getTranslation(path, lang = 'en')
+  strings = STRINGS
+  matches = path.scan(/(\w+)/).flatten
+
+  matches.each do |position|
+    position = position.to_sym
+    strings  = strings.has_key?(position) ? strings[position] : {}
+  end
+
+  lang = 'en' unless strings.has_key?(lang.to_sym)
+
+  strings.empty? ? '' : strings[lang.to_sym]
 end
 
-# testing your program 
-getTranslation('home.intro', 'fr') == 'Bienvenue sur Le Wagon' # => true
-getTranslation('home.intro', 'es') == 'Welcome on Le Wagon' # => true
-getTranslation('home.content.goodbye') == 'Goodbye' # => true
-getTranslation('unvalid.path','en') == '' # => true
+# testing your program
+puts getTranslation('home.intro', 'fr') == 'Bienvenue sur Le Wagon' # => true
+puts getTranslation('home.intro', 'es') == 'Welcome on Le Wagon' # => true
+puts getTranslation('home.content.goodbye') == 'Goodbye' # => true
+puts getTranslation('unvalid.path','en') == '' # => true
+puts getTranslation('home.content.poney', 'en') == ''
