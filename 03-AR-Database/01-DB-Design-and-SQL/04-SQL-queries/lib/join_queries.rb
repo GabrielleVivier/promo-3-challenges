e9b262e6f3a12fd382ca54a0af2c166b9418bd4c
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-24 17:06:26 +0200

    counting occurences

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -1,17 +1,49 @@
 require 'sqlite3'
 
 # opens the database
-DATABASE_PATH = "db/jukebox.sqlite"
+DATABASE_PATH = "lib/db/jukebox.sqlite"
 db = SQLite3::Database.new(DATABASE_PATH)
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
+  db.execute("SELECT Track.Name, Artist.Name, Album.Title
+              FROM Track
+              JOIN Artist ON Artist.ArtistId = Album.ArtistId
+              JOIN Album ON Track.AlbumId = Album.AlbumId")
 end
 
 def stats_on(db, category)
   #TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
+  # category_id = db.execute("SELECT Genre.GenreId
+  #                           FROM Genre
+  #                           WHERE Genre.Name = '#{category}'")
+  array = db.execute("SELECT COUNT(*), AVG(Track.Milliseconds)
+                      FROM Track
+                      JOIN Genre ON Track.GenreId = Genre.GenreId
+                      AND Genre.Name = '#{category}'")
+  hash = Hash.new
+  hash[:category] = category
+  hash[:number_of_songs] = array[0][0]
+  hash[:avg_lenght] = array[0][1]
+  return hash
 end
+
+p stats_on(db, "Latin")
 
 def top_five_rock_artists(db)
   #TODO: return list of top 5 rock artists
+  array = db.execute("SELECT Artist.Name, COUNT(*) occurrences
+              FROM Artist
+              JOIN Album ON Album.ArtistId = Artist.ArtistId
+              JOIN Track ON Track.AlbumId = Album.AlbumId
+              JOIN Genre ON Track.GenreId = Genre.GenreId
+              AND Genre.Name = 'Rock'
+              GROUP BY Artist.Name
+              ORDER BY occurrences DESC
+              LIMIT 5")
+  array.map { |result| result[0] }
 end
+
+p top_five_rock_artists(db)
+
+
