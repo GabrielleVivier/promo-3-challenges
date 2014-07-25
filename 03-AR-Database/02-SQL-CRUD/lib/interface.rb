384a8adcc548a3da9e184b9e2fe615c5ad7f2ebc
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-25 11:25:49 +0200

    done

--- 03-AR-Database/02-SQL-CRUD/lib/interface.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/interface.rb
@@ -5,22 +5,25 @@ def ask_and_get(param)
   gets.chomp
 end
 
-db_path = "db/news.sqlite"
+db_path = "lib/db/news.sqlite"
 db = SQLite3::Database.new(db_path)
 
-create_scheme(db) 
+create_scheme(db)
 
 while true
 
   puts "Hey you, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
   puts "2. Read your posts"
-  puts "3. Delete all posts"
-  puts "4. Exit"
-  
+  puts "3. Read a post"
+  puts "4. Delete all posts"
+  puts "5. Delete a post"
+  puts "6. Update a post"
+  puts "7. Exit"
+
   # TODO: add other CRUD tasks if you wish!
 	choice =  gets.chomp.to_i
-	
+
 	case choice
   when 1
     name = ask_and_get("name")
@@ -31,9 +34,19 @@ while true
   when 2
     get_posts(db)
   when 3
+    id = ask_and_get("id")
+    get_post(db, id)
+  when 4
     delete_posts(db)
-  when 4 
+  when 5
+    id = ask_and_get("id")
+    delete_post(db, id)
+  when 6
+    id = ask_and_get("id")
+    name = ask_and_get("name")
+    update_post(db, id, name)
+  when 7
     break
-	end 
-	
+	end
+
 end
