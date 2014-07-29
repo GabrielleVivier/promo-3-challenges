7f25c98289fb23c6bc76afd1f24f6e33bd9ab0a8
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-07-29 18:04:17 +0200

    done

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Validations/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Validations/lib/interface.rb
@@ -3,3 +3,32 @@ require_relative 'models/post'
 require_relative 'models/user'
 
 # TODO: Add some code to test your validations
+# Test your validation by trying to create duplicated posts
+# or users, users with invalid email,
+# or negative number of ratings.
+
+# user = User.create(name: nil, email: 'hjklk@lncl')
+
+# puts user.valid?
+# puts user.errors.messages
+
+# user1 = User.create(name: "James Brown", email: "gabrielle.vivier@gmail.com")
+
+# puts user1.valid?
+
+# puts user1.errors.messages
+
+# puts User.all
+
+# user2 = User.create(name: "James Brown", email: "james.brown@gmail.com")
+
+# puts user2.valid?
+
+# user3 = User.create(name: "James Blunt", email: "james.blunt@gmail.com")
+
+user = User.find(1)
+
+post1 = user.posts.create(name: "XX", date: Time.now, source_url: "hljbbj.com", rating: 5)
+
+puts post1.valid?
+puts post1.errors.messages
--- 03-AR-Database/04-ActiveRecord-Advanced/03-Validations/lib/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Validations/lib/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :date, presence: true
+  validates :source_url, presence: true
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, presence: true
 end
