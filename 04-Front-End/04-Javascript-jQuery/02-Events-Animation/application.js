4925432b66ea99318abc00696ccba6bc8f0b8ba0
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-08-04 14:40:48 +0200

    hide.slidedown

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,12 @@
   * TODO: add your code here!
   *
 */
+
+$("#message-wrapper").hide().slideDown(1000);
+
+$(function() {
+  $("#message-wrapper").on("click", function(the_event) {
+    $(this).slideUp(1000);
+  });
+});
+
