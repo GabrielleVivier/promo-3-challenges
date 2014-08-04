eea267b551227156e38dea8944babdf36bc675e8
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-08-04 15:59:18 +0200

    event.pageY

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -4,11 +4,26 @@
   *
 */
 
-$("#message-wrapper").hide().slideDown(1000);
+
 
 $(function() {
-  $("#message-wrapper").on("click", function(the_event) {
+
+  $("#message-wrapper").hide().delay(500).slideDown(1000);
+
+  $("#message-wrapper").on("mouseenter", function(the_event) {
+    $(this).slideDown(1000);
+  });
+
+  $("#message-wrapper").on("mouseleave", function(the_event) {
     $(this).slideUp(1000);
   });
+
+  $("body").on("mousemove", function(event) {
+    if (event.pageY < 50) {
+      $("#message-wrapper").slideDown(1000);
+    }
+  });
+
+
 });
 
